120380db89f885e58f4d9ec00399a6fb66e56508
fgwas <805527300@qq.com>
2018-04-19 12:15:56 +0800

    test1

--- js/js2-4.js
+++ js/js2-4.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
     //取上一个页面保存的玩家分配信息
-    var peoplerel = JSON.parse(sessionStorage.getItem("peoplerel"));
+    //获取总人数状态
+    var peoplerel = JSON.parse(sessionStorage.getItem('peoplerel'));
     console.log(peoplerel);
+    //被杀人数
+    var akiller = JSON.parse(sessionStorage.getItem('akiller'));
+    console.log(akiller);
+    //被投票人数
+    var avoter = JSON.parse(sessionStorage.getItem('avoter'));
+    console.log(avoter);
+    //天数days
+    var days = JSON.parse(sessionStorage.getItem('days'));
+    if (days === undefined) {
+        days = 1;
+    }
+    console.log(days);
 
-    //第一天点击滑动下拉框效果
+    var step=sessionStorage.getItem('step');
+    console.log(step);
+
+
+    //点击滑动下拉框效果
     $(".top").click(function () {
         $(".mid").slideToggle("slow");
     });
@@ -14,35 +31,55 @@ $(document).ready(function () {
     });
 
 
+    $('#day').text('第' + days + '天');
+    if (days > 1) {
+        for (var i = 0; i < days - 1; i++) {
+            var day = i + 1;
+            $('#day').text('第' + day + '天');
+        }
+    }
+    console.log(day);
 
-    //创建一个控制阶段的有限状态机$state;
-    var fsm = new StateMachine({
-        init: 'ready',
-        transitions: [
-            {name: 'tokill', form: 'ready', to: 'kill'},
-            {name: 'towords', form: 'kill', to: 'words'},
-            {name: 'tospeak', form: 'words', to: 'speak'},
-            {name: 'tovote', form: 'speak', to: 'vote'},
-            {name: 'toready', form: '*', to: 'ready'}
-        ],
-        methods:{
-            ontokill: function () {
-                alert(1)
-                // $('tte1').click(function () {
-                //     console.log(2);
-                //     alert('开始杀人');
-                //     $('tte').css('background-color','#746b5c');
-                // })
-            },
-            ontte2: function () {
-                $('tte').css('backgroundColor','#746b5c;');
-            },
-            ontte3: function () {
-                $('tte').css('backgroundColor','#746b5c;');
-            },
-            ontte4: function () {
-                $('tte').css('backgroundColor','#746b5c;');
-            }
+
+    // //创建一个控制阶段的有限状态机;
+    // var fsm = new StateMachine({
+    //     init: 'none',
+    //     transitions: [
+    //         {name: 'kill', form: 'none', to: 'kill'},
+    //         {name: 'words', form: 'kill', to: 'words'},
+    //         {name: 'speak', form: 'words', to: 'speak'},
+    //         {name: 'vote', form: 'speak', to: 'vote'},
+    //         {name: 'goto', form: '*', to: 'none'}
+    //     ],
+    //     methods: {
+    //         onAfterKill: function (lifecycle) {
+    //             console.log(123123123);
+    //             var stateDie = fsm.state;
+    //             localStorage.setItem('stateDie', stateDie);
+    //             $('#tokill').css('backgroundColor', '#18758D');
+    //
+    //
+    //         },
+    //         wordsPass: function () {
+    //             $('tte').css('backgroundColor', '#746b5c;');
+    //         },
+    //         speakPass: function () {
+    //             $('tte').css('backgroundColor', '#746b5c;');
+    //         },
+    //         votePass: function () {
+    //             $('tte').css('backgroundColor', '#746b5c;');
+    //         }
+    //     }
+    // })
+
+    $('#tokill').click(function () {
+        if(step===1){
+            alert("你今天已经杀过人了！");
+        }else {
+            alert('你准备好了吗？');
+            window.location.href='法官日志.html';
+            $('#tokill').css('background','#18758D');
+
         }
     })
 
--- js/法官日志.js
+++ js/法官日志.js
@@ -3,16 +3,35 @@ $(document).ready(function () {
     var peoplerel = JSON.parse(sessionStorage.getItem("peoplerel"));
     console.log(peoplerel);
 
-    window.onload=function () {
-     for(i=0;i<peoplerel.length;i++){
-         var tt1=$('<div></div>').text(peoplerel[i]).addClass('top');
-         var tt2=$('<div></div>').text(i+1).addClass('mid');
-         var tt3=$('<div></div>').addClass('box');
-        $('main').append(tt3);
-        $(tt3).append(tt1,tt2);
-     }
+    days = 1;//初始化游戏天数
+    akiller = [];//存放被杀玩家
+    avoter = [];//存放被投票玩家
 
-    }
+    sessionStorage.setItem('days', JSON.stringify(days));
+    sessionStorage.setItem('akiller', JSON.stringify(akiller));
+    sessionStorage.setItem('avoter', JSON.stringify(avoter));
+    sessionStorage.setItem('step', 'none');
+
+    window.onload = function () {
+        for (var i = 0; i < peoplerel.length; i++) {
+            var num = i + 1;
+            var tt1 = $('<div></div>').text(peoplerel[i]).addClass('top');
+            var tt2 = $('<div></div>').text(num + '号').addClass('mid');
+            var tt3 = $('<div></div>').addClass('box');
+            $('main').append(tt3);
+            $(tt3).append(tt1, tt2);
+
+
+        }
+    };
+    window.sessionStorage.setItem('peoplerel', JSON.stringify(peoplerel));
+
+    $('.box').click(function () {
+
+        $('.top').css('background', 'black');
+        $('.top').css('color', 'white');
+    })
+
 
 });
 
